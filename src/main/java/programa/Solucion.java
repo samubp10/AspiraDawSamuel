77bafdab2014198eb3cbfdd118c30444ad38a562
samuel <samubp10@gmail.com>
2020-11-14 19:43:36 +0100

    Crea el array que contiene las dependencias y crea otro array el cuál va rellenando el usuario

--- src/main/java/programa/Solucion.java
+++ src/main/java/programa/Solucion.java
@@ -20,6 +20,10 @@ public class Solucion {
         String contrasenaAuten;
         String valorCarga;
         double carga;
+        String[] dependencias = {"Cocina", "Salón", "Cuarto de baño", "Dormitorio 1", "Dormitorio 2"};
+        int m2dependencia = 0;
+        int[] m2dependencias = new int[5];
+        int i;
         do {
             usuarioAuten = JOptionPane.showInputDialog("Introduzca el nombre de usuario");
             contrasenaAuten = JOptionPane.showInputDialog("Introduzca la contraseña");
@@ -28,6 +32,14 @@ public class Solucion {
             valorCarga = JOptionPane.showInputDialog("Introduzca la carga de la aspiradora");
             carga = Double.parseDouble(valorCarga);
         } while (carga < 0 || carga > 100);
-
+        for (i = 0; i < dependencias.length; i++) {
+            do {
+                String m2 = JOptionPane.showInputDialog("Introduzca el número de metros al cuadrado que tiene la dependencia "
+                        + dependencias[i]);
+                m2dependencia = Integer.parseInt(m2);
+                m2dependencias[i] = (int) m2dependencia;
+                System.out.println(dependencias[i] + " " + m2dependencias[i]);
+            } while (m2dependencia < 1 || m2dependencia > 100);
+        }
     }
 }
